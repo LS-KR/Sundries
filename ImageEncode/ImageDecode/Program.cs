621613bd2e3b8c29a828b07bcb850c5340ddd856
LS-KR <Elihuso@outlook.com>
2023-07-16 19:32:02 +0800

    [+] Add ImageEncode

---
+++ ImageEncode/ImageDecode/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageDecode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filesource = "";
+            if (args.Length == 0)
+            {
+                Console.Write("Input the source of the file you want to decrypt...\n> ");
+                filesource = Console.ReadLine();
+            }
+            else
+                filesource = args[0];
+            try
+            {
+                byte[] head =
+                {
+                    0x42, 0x4d, 0x36, 0x04,
+                    0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x36, 0x04, 0x00, 0x00,
+                    0x28, 0x00, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00,
+                    0x01, 0x00, 0x08, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00
+                };
+                byte[] bts = File.ReadAllBytes(filesource);
+                List<byte> lbfname = new List<byte>();
+                List<byte> content = new List<byte>();
+                int i = head.Length;
+                while (true)
+                {
+                    if ((bts[i] == 0x00) && (bts[i + 1] == 0x00))
+                        break;
+                    lbfname.Add(bts[i]);
+                    i++;
+                }
+                i += 2;
+                while (i < bts.Length)
+                {
+                    content.Add(bts[i]);
+                    i++;
+                }
+                string fname = Encoding.UTF8.GetString(lbfname.ToArray());
+                File.WriteAllBytes(fname, content.ToArray());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ ImageEncode/ImageEncode/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageEncode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filesource = "";
+            if (args.Length == 0)
+            {
+                Console.Write("Input the source of the file you want to encrypt...\n> ");
+                filesource = Console.ReadLine();
+            }
+            else
+                filesource = args[0];
+            try
+            {
+                byte[] bts = File.ReadAllBytes(filesource);
+                string[] fname = filesource.Split(new char[] { '\\', '/' });
+                byte[] bfname = Encoding.UTF8.GetBytes(fname[fname.Length - 1]);
+                byte[] head =
+                {
+                    0x42, 0x4d, 0x36, 0x04,
+                    0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x36, 0x04, 0x00, 0x00,
+                    0x28, 0x00, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00,
+                    0x01, 0x00, 0x08, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00,
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00
+                };
+                List<byte> outputlist = new List<byte>();
+                Console.WriteLine("Writing Head...");
+                for (int i = 0; i < head.Length; i++)
+                    outputlist.Add(head[i]);
+                for (int j = 0; j < bfname.Length; j++)
+                    outputlist.Add(bfname[j]);
+                outputlist.Add(0x00);
+                outputlist.Add(0x00);
+                Console.WriteLine("Generating...");
+                for (int k = 0; k < bts.Length; k++)
+                    outputlist.Add(bts[k]);
+                File.WriteAllBytes("output.bmp", outputlist.ToArray());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
