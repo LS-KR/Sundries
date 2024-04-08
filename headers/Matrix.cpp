df9f2f91a8c61dccc44bc92e46467ba1672d0e3d
Elihuso Quigley <Elihuso@outlook.com>
2024-04-08 18:31:19 +0800

    [F] Use unsigned int

--- headers/Matrix.cpp
+++ headers/Matrix.cpp
@@ -40,8 +40,8 @@ Matrix Matrix::operator+(Matrix m) {
     if ((this->cols != m.cols) || (this->rows != m.rows))
         throw std::invalid_argument("The number of rows and columns of the matrix is not match");
     Matrix result(this->rows, this->cols);
-    for (int i = 0; i < this->rows; ++i) {
-        for (int j = 0; j < this->cols; ++j) {
+    for (unsigned int i = 0; i < this->rows; ++i) {
+        for (unsigned int j = 0; j < this->cols; ++j) {
             result.Set(i, j, this->data[i * this->rows + j] + m.data[i * m.rows + j]);
         }
     }
@@ -52,8 +52,8 @@ Matrix Matrix::operator-(Matrix m) {
     if ((this->cols != m.cols) || (this->rows != m.rows))
         throw std::invalid_argument("The number of rows and columns of the matrix is not match");
     Matrix result(this->rows, this->cols);
-    for (int i = 0; i < this->rows; ++i) {
-        for (int j = 0; j < this->cols; ++j) {
+    for (unsigned int i = 0; i < this->rows; ++i) {
+        for (unsigned int j = 0; j < this->cols; ++j) {
             result.Set(i, j, this->data[i * this->rows + j] - m.data[i * m.rows + j]);
         }
     }
@@ -64,10 +64,10 @@ Matrix Matrix::operator*(Matrix m) {
     unsigned int r = this->rows;
     unsigned int c = m.cols;
     Matrix result(r, c);
-    for (int i = 0; i < r; ++i) {
-        for (int j = 0; j < c; ++j) {
+    for (unsigned int i = 0; i < r; ++i) {
+        for (unsigned int j = 0; j < c; ++j) {
             double number = 0;
-            int k = 0;
+            unsigned int k = 0;
             do {
                 number += this->Get(i, k) * m.Get(k, j);
             } while((++k) < ((this->getCols() < m.getRows()) ? this->getCols() : m.getRows()));
@@ -80,8 +80,8 @@ Matrix Matrix::operator*(Matrix m) {
 bool Matrix::operator==(Matrix m) {
     if ((this->cols != m.cols) || (this->rows != m.rows))
         throw std::invalid_argument("The number of rows and columns of the matrix is not match");
-    for (int i = 0; i < m.getRows(); ++i) {
-        for (int j = 0; j < m.getCols(); ++j) {
+    for (unsigned int i = 0; i < m.getRows(); ++i) {
+        for (unsigned int j = 0; j < m.getCols(); ++j) {
             if (this->Get(i, j) != m.Get(i, j)) return false;
         }
     }
